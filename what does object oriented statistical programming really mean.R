420ebb8517c9cdb72f59936b65dbfd374291091b
Lin HsaunChun <lin.hsuan.chun@gmail.com>
2015-05-11 07:01:12 +0800

    物件資訊練習
    範例1:
    創建一個矩陣x，其元素值從200到214
    範例2:
    查詢x矩陣的長度
    範例3:
    查詢x矩陣的類型(文字,數字,邏輯值)
    範例4:
    查詢x矩陣第3個元素的值
    範例5:
    查詢x矩陣第20個元素的值
    範例6:
    將x矩陣中位置3到8的元素存入y矩陣

---
+++ what does object oriented statistical programming really mean.R
@@ -0,0 +1,13 @@
+#creat x incloud 200 to 214.
+x<-200:214
+#find out how length with x.
+length(x)
+#find out x class.
+class(x)
+#read value of position 3 in x.
+x[3]
+#try to find value of position 20.
+x[20]
+  #this is beacuse length of x only 15.
+#creat Val y to incould position 3 to 8 in x
+y<-x[3:8]
