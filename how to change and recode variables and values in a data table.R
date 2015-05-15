d7ca13ca6cd860fce0bd7663b161620f2476064b
Lin HsaunChun <lin.hsuan.chun@gmail.com>
2015-05-15 15:35:12 +0800

    如何修改表格內資料
    1.fix( ) 用GUI界面修改
    2.新增欄位
    3.新增欄位且值為另一個欄位的倍數
    killometers.pre.litter=0.425143707*mpg

--- how to change and recode variables and values in a data table.R
+++ how to change and recode variables and values in a data table.R
@@ -1,2 +1,31 @@
+data(mtcars)
+
+mtcars
+#用GUI界面來看mtcars中有什麼資料並可直接點即修改
 fix(mtcars)
 #在mtcars新增一個欄位killometers.pre.litter而且killometers.pre.litter的值為0.425143707*mpg
+mtcars<- transform(mtcars, killometers.pre.litter = 0.425143707*mpg)
+#在mtcars新增兩個欄位killometers.pre.litter,wattage和wattage。
+#killometers.pre.litter,wattage的值為0.425143707*mpg且wattage的值為hp*746
+mtcars<-
+  transform(mtcars, 
+            killometers.pre.litter = 0.425143707*mpg,
+            wattage = hp*746
+           )
+
+
+mtcars[,"killometers.pre.litter"]<-
+  mtcars[,"mpg"]*0.425143707
+
+mtcars[,"wattage"]<-
+  mtcars[,"hp"]*746
+
+
+data(mtcars)
+
+mtcars[ ,"am"]==1
+
+mtcars[ mtcars[ ,"am"]==1, "wattage"]<-
+  mtcars[ mtcars[ ,"am"]==1, "hp"]*746
+
+mtcars
